6a74e9248376bba64de0130259741368399df768
Sunny Gupta <e.sunnygupta@gmail.com>
2022-01-09 02:48:27 +0530

    Integration of MPV Player with the app.
    libmpv is compiled externally for x86.

---
+++ src/player/include/player.h
@@ -0,0 +1,16 @@
+/*
+ * player.h
+ *
+ *  Created on: 01-Oct-2018
+ *      Author: sunny
+ */
+
+#ifndef SRC_PLAYER_INCLUDE_PLAYER_H_
+#define SRC_PLAYER_INCLUDE_PLAYER_H_
+
+#include <client.h>
+#include <render_gl.h>
+
+int mpv_main(char *message, mpv_handle *mpv, mpv_render_context *mpv_gl);
+
+#endif /* SRC_PLAYER_INCLUDE_PLAYER_H_ */
